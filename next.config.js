337a931b504d1e537960f180c1da3fa327db4ab3
kim-taewoo <acoustic0419@gmail.com>
2022-08-02 01:49:53 +0900

    fix pwa setting based on https://github.com/shadowwalker/next-pwa/issues/295

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = {
   // OAuth 요청 등도 2번씩 보내고 있어서... 일단 끔
@@ -16,10 +17,13 @@ module.exports = withPlugins(
     [
       withPWA,
       {
+        reactStrictMode: true,
         pwa: {
           dest: '/public',
           register: true,
           skipWaiting: true,
+          runtimeCaching,
+          buildExcludes: [/middleware-manifest.json$/],
           disable: process.env.NODE_ENV === 'development',
         },
       },
